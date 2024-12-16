2f3e3b5bcd804471f352bd197b35e50501e959a8
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-12-16 18:48:55 +0200

    Add comments

--- src/main/java/pat/Task3.java
+++ src/main/java/pat/Task3.java
@@ -11,13 +11,13 @@ public class Task3 {
         int sum = 0;
         BigInteger fact = BigInteger.ONE;
 
-        for (int i = 1; i <= 100; i++) {
-            fact = fact.multiply(BigInteger.valueOf(i));
+        for (int i = 1; i <= 100; i++) { // iteration method factorial
+            fact = fact.multiply(BigInteger.valueOf(i)); // multiply
         }
 
-        String factString = fact.toString();
+        String factString = fact.toString(); // BigInteger to string, because we need process numbers one by one
         for (int i = 0; i < factString.length(); i++) {
-            sum += Integer.parseInt(factString.charAt(i) + "");
+            sum += Integer.parseInt(factString.charAt(i) + ""); // increase sum
         }
 
         return sum;
